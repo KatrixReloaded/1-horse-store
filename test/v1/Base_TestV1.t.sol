94d6ee196f767cf269fc0a9b02ddfac465c777c9
KatrixReloaded <kartikeyasureka@gmail.com>
2024-12-29 19:39:01 +0530

    This is supposed to crash

--- test/v1/Base_TestV1.t.sol
+++ test/v1/Base_TestV1.t.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.20;
 
-import { Test, console2 } from "../../lib/forge-std/Test.sol";
+import { Test, console2 } from "forge-std/Test.sol";
 import { HorseStore } from "../../src/horseStoreV1/HorseStore.sol";
 
 abstract contract Base_TestV1 is Test {
